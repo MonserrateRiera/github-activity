d3edf1c4aa05532b27773e3535206d25966c51fc
Monserrate Riera <mrieramar@uoc.edu>
2025-02-16 20:23:15 +0100

    Added the  HttpURLConnection and send a request.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,30 @@
-
-
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("ERROR: There arent any arguments!");
+        }else{
+            try {
 
+                URL url = new URL("https://api.github.com/users/"+ args[0] +"/events") ;
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                int status = connection.getResponseCode();
+                System.out.println(status);
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (ProtocolException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
